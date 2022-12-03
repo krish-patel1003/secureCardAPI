0c40fff3d351c02b61c1011e2b5f537b36b45b54
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2022-12-03 11:02:36 +0530

    IsMerchant permission class added

--- checkout/permissions.py
+++ checkout/permissions.py
@@ -1,8 +1,12 @@
 from rest_framework import permissions
+from rest_framework import exceptions
 
 class IsMerchant(permissions.BasePermission):
     
     def has_object_permission(self, request, view, obj):
         logged_in_user = request.user
-        return logged_in_user.role == "MERCHANT" and obj.merchant == logged_in_user
- 
+        if logged_in_user.role != "Merchant":
+            raise exceptions.PermissionDenied({"Error": "You should be a merchant to access this endpoint"})
+        
+        return True
+        
