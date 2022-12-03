9a22af70382fd4cc9f7af8b60546e9ae30cf0898
krish-patel1003 <92010868+krish-patel1003@users.noreply.github.com>
2022-12-03 10:50:14 +0530

    changes

--- checkout/permissions.py
+++ checkout/permissions.py
@@ -1,4 +1,8 @@
 from rest_framework import permissions
 
 class IsMerchant(permissions.BasePermission):
-    pass
+    
+    def has_object_permission(self, request, view, obj):
+        logged_in_user = request.user
+        return logged_in_user.role == "MERCHANT" and obj.merchant == logged_in_user
+ 
